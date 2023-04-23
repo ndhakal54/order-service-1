267a5e5b3937820e9013d732beed2a2bb9db053e
Nishan Dhakal <ndhakal54@outlook.com>
2023-04-23 00:30:44 -0500

    Updated OrderServiceApplication.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.javatechie.aws.cicd.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,14 @@ public class OrderServiceApplication {
     public List<Order> fetchOrders() {
         return orderDao.getOrders().stream().
                 sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/price")
+    public String ViewForm(Model model) {
+        int a = 7;
+        int b = 8;
+
+        return "Sum of " + a + " and " + b + " is = " + (a+b);
     }
 
     public static void main(String[] args) {
